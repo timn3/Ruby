30c293ec7812818d443ee805024e8e66703ee485
Damian Niemczyk <djniemczyk7@googlemail.com>
2017-03-02 17:05:05 +0100

    Tabs entfernen und richtig indentieren
    2 Spaces

--- encoder_hexa_v_4.rb
+++ encoder_hexa_v_4.rb
@@ -7,19 +7,19 @@ $encoded = Array.new
 enc = gets.chomp.split("")
 
 def letter (character)
-	case character
-		when $csv_array[$csv_array_y][$csv_array_x]
-			$encoded << $csv_array[$csv_array_y][$csv_array_x += 1]
-			$input_array_index += 1
-			$csv_array_y = 0
-			$csv_array_x = 0
-		else
-			$csv_array_y += 1
-	end
+  case character
+  when $csv_array[$csv_array_y][$csv_array_x]
+    $encoded << $csv_array[$csv_array_y][$csv_array_x += 1]
+    $input_array_index += 1
+    $csv_array_y = 0
+    $csv_array_x = 0
+  else
+    $csv_array_y += 1
+  end
 end
 
 while $input_array_index < enc.length
-		letter enc[$input_array_index]
+  letter enc[$input_array_index]
 end
 output = $encoded.join(" ")
 File.open("data/output.txt", "w") {|f| f.write output}

504e451de3d6e0149f50cfce75176ed8c9938743
tim <tim@asus-chi>
2017-07-08 18:27:11 +0200

    Update

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,6 @@
-arr = Array.new
-num = 0
-while num < 100
-	num += 1
-	if num%3 == 0 && num%5 == 0 
-		arr.push("FizzBuzz")
-	elsif num%3 == 0
-		arr.push("Fizz")
-	elsif num%5 == 0
-		arr.push("Buzz")
-	else
-		arr.push(num)
-	end
+for num in (1..100) 
+  say = ""
+  say << "Fizz" if num % 3 == 0
+  say << "Buzz" if num % 5 == 0
+  if say == "" then puts num else puts say end 
 end
-puts arr
